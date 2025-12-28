48cac5b72485d0faba39c01a11e2eb31f77cf236
Prembhardwaj001 <pktech291@gmail.com>
2025-12-28 10:05:09 +0530

    Add files via upload

---
+++ assigment1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+
+# addition + :-
+print("addition is: ", num1 + num2)
+# subtraction - :-
+print("subtraction is: ", num1 - num2)
+# multiplication * :-
+print("multiplication is: ", num1 * num2)
+# division / :-
+print("division is: ", num1 / num2)
---
+++ assigment2.py
@@ -0,0 +1,4 @@
+a = input ("Enter a name: ")
+b = input ("Enter second name: ")
+
+print("hello," + a + " " + b + "welocome to python program")
