5eba62e5de8e86d91e07bda4787fa00043108d74
Prembhardwaj001 <pktech291@gmail.com>
2025-12-28 10:09:39 +0530

    Correct input prompts and fix spelling in output
    Fixed input prompts and corrected spelling errors in output message.

--- assigment2.py
+++ assigment2.py
@@ -2,3 +2,9 @@ a = input ("Enter a name: ")
 b = input ("Enter second name: ")
 
 print("hello," + a + " " + b + "welocome to python program")
+
+enter your name:alex
+enter your second name:black
+Hello, alex black welocme to programe python
+
+Process finished with exit code 0
