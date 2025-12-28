1dfdbb8f6f0d70e65c8461f9235ed420b871fa33
Prembhardwaj001 <pktech291@gmail.com>
2025-12-28 10:10:51 +0530

    Fix formatting and add missing newline in assignment1.py

--- assigment1.py
+++ assigment1.py
@@ -1,7 +1,6 @@
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 
-
 # addition + :-
 print("addition is: ", num1 + num2)
 # subtraction - :-
@@ -10,3 +9,10 @@ print("subtraction is: ", num1 - num2)
 print("multiplication is: ", num1 * num2)
 # division / :-
 print("division is: ", num1 / num2)
+
+Enter a number: 10
+Enter another number: 20
+addition is:  30
+multiplication is:  200
+division is:  0.5
+substraction is:  -10
