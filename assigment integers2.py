6a8b1d5518e77c99111da581d817c277ffecd1bb
Prembhardwaj001 <pktech291@gmail.com>
2026-01-06 08:59:44 +0530

    Add files via upload

---
+++ assigment integers2.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total = 0
+
+# Loop from 1 to 50
+for i in range(1, 51):
+    total = total + i
+
+# Display the final sum
+print("The sum of integers from 1 to 50 is:", total)
