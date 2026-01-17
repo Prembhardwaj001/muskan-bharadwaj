a6103b7bec748e51743253c5c674370037f341bc
Prembhardwaj001 <pktech291@gmail.com>
2026-01-17 09:40:36 +0530

    Add files via upload

---
+++ assigment module1.py
@@ -0,0 +1,14 @@
+def factorial(num):
+    fact = 1
+    for i in range(1, num + 1):
+        fact = fact * i
+    return fact
+
+# Sample number
+number = 5
+
+# Function call
+result = factorial(number)
+
+# Output
+print("Factorial of", number, "is:", result)
