b64ba3e5a9f3dc26e900c8466d215c987cb5053f
Prembhardwaj001 <pktech291@gmail.com>
2026-01-17 09:50:26 +0530

    Reformat code in assignment module2.py

--- assigment module2.py
+++ assigment module2.py
@@ -12,3 +12,10 @@ sine_value = math.sin(number)
 print("Square root of the number:", square_root)
 print("Natural logarithm (log base e):", natural_log)
 print("Sine of the number (in radians):", sine_value)
+
+Enter a number: 12
+Square root of the number: 3.4641016151377544
+Natural logarithm (log base e): 2.4849066497880004
+Sine of the number (in radians): -0.5365729180004349
+
+Process finished with exit code 0
